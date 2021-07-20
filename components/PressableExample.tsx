4512bdaf98e2eba4200b25e7de5e568760a97f09
Grant Sander <gksander93@gmail.com>
2021-07-19 20:48:03 -0500

    Update PressableExample.tsx

--- components/PressableExample.tsx
+++ components/PressableExample.tsx
@@ -9,55 +9,42 @@ import {
   View,
   ViewStyle,
   Animated,
+  StyleSheet,
 } from 'react-native';
-import Reanimated, {
-  interpolate,
-  useAnimatedStyle,
-  useSharedValue,
-  Extrapolate,
-  withTiming,
-} from 'react-native-reanimated';
 import { Spacer } from './Spacer';
 
 export const PressableExample: React.FC = () => {
+  // Inner content
+  const content = (
+    <React.Fragment>
+      <Image
+        source={{ uri: 'https://source.unsplash.com/daily' }}
+        style={{
+          width: 75,
+          height: 75,
+          resizeMode: 'cover',
+          borderRadius: 75 / 2,
+        }}
+      />
+      <Spacer width={16} />
+      <View>
+        <Text style={{ fontWeight: '700', fontSize: 24 }}>Daily Photo</Text>
+        <Text style={{ fontWeight: '300', fontSize: 13 }}>
+          From Source Unsplash
+        </Text>
+      </View>
+    </React.Fragment>
+  );
+
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
-      {[
-        MyTouchableOpacity,
-        MyTouchableOpacityAnimated,
-        MyTouchableOpacityReanimated,
-      ].map((TouchableOpacity, i) => (
-        <React.Fragment key={i}>
-          <TouchableOpacity
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              padding: 16,
-              backgroundColor: '#ededed',
-            }}
-            onPress={() => null}>
-            <Image
-              source={{ uri: 'https://source.unsplash.com/daily' }}
-              style={{
-                width: 75,
-                height: 75,
-                resizeMode: 'cover',
-                borderRadius: 75 / 2,
-              }}
-            />
-            <Spacer width={16} />
-            <View>
-              <Text style={{ fontWeight: '700', fontSize: 24 }}>
-                Daily Photo
-              </Text>
-              <Text style={{ fontWeight: '300', fontSize: 13 }}>
-                From Source Unsplash
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <Spacer height={32} />
-        </React.Fragment>
-      ))}
+      <PlainJaneTouchableOpacity style={styles.container} onPress={() => null}>
+        {content}
+      </PlainJaneTouchableOpacity>
+      <Spacer height={50} />
+      <AnimatedTouchableOpacity style={styles.container} onPress={() => null}>
+        {content}
+      </AnimatedTouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -69,7 +56,7 @@ type MyTouchableOpacityProps = Omit<PressableProps, 'style'> & {
 /**
  * Basic version
  */
-const MyTouchableOpacity: React.FC<MyTouchableOpacityProps> = ({
+const PlainJaneTouchableOpacity: React.FC<MyTouchableOpacityProps> = ({
   style,
   ...rest
 }) => {
@@ -87,8 +74,10 @@ const MyTouchableOpacity: React.FC<MyTouchableOpacityProps> = ({
 /**
  * Animated with RN Animated API
  */
-const MyTouchableOpacityAnimated: React.FC<MyTouchableOpacityProps> = ({
+const AnimatedTouchableOpacity: React.FC<MyTouchableOpacityProps> = ({
   style,
+  onPressIn,
+  onPressOut,
   ...rest
 }) => {
   const pressInProgress = React.useRef(new Animated.Value(0)).current;
@@ -102,20 +91,20 @@ const MyTouchableOpacityAnimated: React.FC<MyTouchableOpacityProps> = ({
   return (
     <AnimatedPressable
       onPressIn={evt => {
+        onPressIn?.(evt);
         Animated.timing(pressInProgress, {
           toValue: 1,
           useNativeDriver: true,
           duration: 150,
         }).start();
-        rest?.onPressIn?.(evt);
       }}
       onPressOut={evt => {
+        onPressOut?.(evt);
         Animated.timing(pressInProgress, {
           toValue: 0,
           useNativeDriver: true,
           duration: 150,
         }).start();
-        rest?.onPressOut?.(evt);
       }}
       style={[...(Array.isArray(style) ? style : [style]), { opacity }]}
       {...rest}
@@ -125,40 +114,11 @@ const MyTouchableOpacityAnimated: React.FC<MyTouchableOpacityProps> = ({
 
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
-/**
- * Animated with Reanimated 2
- */
-const MyTouchableOpacityReanimated: React.FC<MyTouchableOpacityProps> = ({
-  style,
-  ...rest
-}) => {
-  const pressInProgress = useSharedValue(0);
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(
-        pressInProgress.value,
-        [0, 1],
-        [1, 0.3],
-        Extrapolate.CLAMP,
-      ),
-    };
-  });
-
-  return (
-    <ReanimatedPressable
-      onPressIn={evt => {
-        pressInProgress.value = withTiming(1, { duration: 150 });
-        rest?.onPressIn?.(evt);
-      }}
-      onPressOut={evt => {
-        pressInProgress.value = withTiming(0, { duration: 150 });
-        rest?.onPressOut?.(evt);
-      }}
-      style={[...(Array.isArray(style) ? style : [style]), animatedStyle]}
-      {...rest}
-    />
-  );
-};
-
-const ReanimatedPressable = Reanimated.createAnimatedComponent(Pressable);
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#ededed',
+  },
+});
