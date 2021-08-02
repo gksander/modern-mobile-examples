cd352b564537e1c2169d464aa4565dd30eba5ff8
Grant Sander <gksander93@gmail.com>
2021-08-02 12:42:37 -0500

    IG search layout animation snippet

--- components/SearchBarExample.tsx
+++ components/SearchBarExample.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import {
+  FlatList,
+  ListRenderItem,
   SafeAreaView,
+  StatusBar,
+  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
@@ -14,8 +18,18 @@ import Animated, {
   Layout,
   FadeInLeft,
   FadeOutLeft,
+  FadeInRight,
+  FadeOutRight,
+  SlideInRight,
+  SlideOutRight,
+  FadeInUp,
+  FadeOutUp,
+  FadeInDown,
+  FadeOutDown,
 } from 'react-native-reanimated';
-import { ts } from '../utils/styles';
+import Icon from 'react-native-vector-icons/Feather';
+import { constraints, ts } from '../utils/styles';
+import { BetterImage } from './BetterImage';
 
 type SearchBarExampleProps = {};
 
@@ -24,51 +38,128 @@ export const SearchBarExample: React.FC<SearchBarExampleProps> = () => {
   const textRef = React.useRef<TextInput>(null);
 
   return (
-    <SafeAreaView style={ts('flex:1')}>
-      <View style={ts('flex:1')}>
-        <AnimatedLayout>
-          <View style={ts('p:2')}>
-            {!isFocused && (
+    <React.Fragment>
+      <AnimatedLayout style={ts('flex:1')}>
+        <View style={ts('flex:1')}>
+          {/* Header */}
+          <View style={ts('bg:black', 'py:2')}>
+            <SafeAreaView>
               <Animated.View
-                style={ts('flex:row', 'justify:between', 'pb:2')}
-                entering={FadeIn.duration(200).delay(100)}
-                exiting={FadeOut.duration(200)}>
-                <Text>BRAND</Text>
-                <Text>Account</Text>
+                style={ts('flex:row', 'items:center', 'px:2')}
+                layout={Layout.duration(200).easing(Easing.ease)}>
+                {!isFocused && (
+                  <AnimatedTouchable
+                    style={ts('mr:2')}
+                    entering={FadeInLeft.duration(200).easing(Easing.ease)}
+                    exiting={FadeOutLeft.duration(200).easing(Easing.ease)}>
+                    <Icon
+                      name="user-plus"
+                      size={18}
+                      color={constraints.colors.white}
+                    />
+                  </AnimatedTouchable>
+                )}
+                <Animated.View
+                  layout={Layout.duration(200).easing(Easing.ease)}
+                  style={ts(
+                    'flex:1',
+                    'flex:row',
+                    'items:center',
+                    'rounded:base',
+                    'bg:gray-800',
+                  )}>
+                  <View style={ts('h:full', 'p:2', 'pr:0')}>
+                    <Icon
+                      name="search"
+                      color={constraints.colors['gray-500']}
+                    />
+                  </View>
+                  <TextInput
+                    onFocus={() => {
+                      setIsFocused(true);
+                    }}
+                    onBlur={() => {
+                      setIsFocused(false);
+                    }}
+                    style={ts('p:2', 'flex:grow', 'color:white')}
+                    ref={textRef}
+                    placeholder="Search"
+                    placeholderTextColor={constraints.colors['gray-500']}
+                  />
+                </Animated.View>
+                {isFocused && (
+                  <AnimatedTouchable
+                    style={ts('ml:2')}
+                    entering={FadeInDown.duration(200).easing(Easing.ease)}
+                    exiting={FadeOutDown.duration(100).easing(Easing.ease)}
+                    onPress={() => {
+                      textRef?.current?.blur?.();
+                    }}>
+                    <Text style={ts('color:white', 'text:sm')}>Cancel</Text>
+                  </AnimatedTouchable>
+                )}
+              </Animated.View>
+            </SafeAreaView>
+          </View>
+
+          {/* Body */}
+          <View style={ts('flex:1', 'bg:gray-500')}>
+            <FlatList
+              data={IDS}
+              renderItem={renderItem}
+              keyExtractor={keyExtractor}
+              numColumns={2}
+            />
+
+            {isFocused && (
+              <Animated.View
+                style={ts('absolute', 'inset:0', 'bg:black', 'py:24')}
+                entering={FadeInDown.duration(200).easing(Easing.ease)}
+                exiting={FadeOutDown.duration(200).easing(Easing.ease)}>
+                <Text
+                  style={ts(
+                    'color:white',
+                    'text-align:center',
+                    'text:xl',
+                    'font-weight:bold',
+                  )}>
+                  SEARCH RESULTS
+                </Text>
               </Animated.View>
             )}
-            <Animated.View
-              style={ts('flex:row', 'items:center')}
-              layout={Layout.duration(200).easing(Easing.ease)}>
-              {isFocused && (
-                <AnimatedTouchable
-                  style={ts('pr:2')}
-                  entering={FadeInLeft.duration(200)}
-                  exiting={FadeOutLeft.duration(200)}
-                  onPress={() => {
-                    textRef?.current?.blur?.();
-                  }}>
-                  <Text>Back</Text>
-                </AnimatedTouchable>
-              )}
-              <AnimatedTextInput
-                onFocus={() => {
-                  setIsFocused(true);
-                }}
-                onBlur={() => {
-                  setIsFocused(false);
-                }}
-                style={ts('border:hairline', 'p:2', 'flex:grow')}
-                layout={Layout.duration(200)}
-                ref={textRef}
-              />
-            </Animated.View>
           </View>
-        </AnimatedLayout>
-      </View>
-    </SafeAreaView>
+        </View>
+      </AnimatedLayout>
+
+      <StatusBar barStyle="light-content" />
+    </React.Fragment>
   );
 };
+
+const IDS = Array(20)
+  .fill(null)
+  .map(_ => 1 + Math.floor(10000 * Math.random()));
+
+const renderItem: ListRenderItem<number> = ({ item }) => {
+  return (
+    <View style={{ flex: 0.5 }}>
+      <View
+        style={[
+          ts('w:full', 'aspect:1'),
+          { padding: StyleSheet.hairlineWidth },
+        ]}>
+        <BetterImage
+          source={{
+            uri: `https://source.unsplash.com/random/640x320?sig=${item}`,
+          }}
+          style={ts('w:full', 'h:full')}
+          resizeMode="cover"
+        />
+      </View>
+    </View>
+  );
+};
+const keyExtractor = (item: number) => String(item);
 
 const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
